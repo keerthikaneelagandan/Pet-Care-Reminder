0bb74d77ef597aff1d18067c728832caea9bfbb5
Keerthika Neelagandan <144517207+keerthikaneelagandan@users.noreply.github.com>
2025-09-04 13:31:20 +0530

    Update pet_care_db.sql

--- pet_care_db.sql
+++ pet_care_db.sql
@@ -1,53 +1,24 @@
--- MySQL dump 10.13  Distrib 8.0.43, for Win64 (x86_64)
---
--- Host: localhost    Database: pet_care_db
--- ------------------------------------------------------
--- Server version	8.0.43
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE IF NOT EXISTS petcarereminder;
+USE petcarereminder;
 
---
--- Table structure for table `reminders`
---
+CREATE TABLE IF NOT EXISTS reminders (
+  pet_id INT PRIMARY KEY AUTO_INCREMENT,
+  pet_name VARCHAR(50) NOT NULL,
+  pet_type VARCHAR(50) NOT NULL,
+  reminder_type VARCHAR(50) NOT NULL,  
+  reminder_date DATE NOT NULL,
+  description VARCHAR(100)
+);
+INSERT INTO reminders( pet_name, pet_type,reminder_type, reminder_date)
+VALUES
+('Pinky', 'cat', 'Vet Visit', '2025-09-05'),
+('Nimme', 'cat', 'Vaccination', '2025-09-10'),
+('Blacky','dog', 'Deworming', '2025-09-15');
 
-DROP TABLE IF EXISTS `reminders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `reminders` (
-  `pet_id` int NOT NULL AUTO_INCREMENT,
-  `pet_name` varchar(50) NOT NULL,
-  `pet_type` varchar(50) NOT NULL,
-  `reminder_date` date NOT NULL,
-  `description` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`pet_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+SELECT * FROM reminders;
 
---
--- Dumping data for table `reminders`
---
-
-LOCK TABLES `reminders` WRITE;
-/*!40000 ALTER TABLE `reminders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `reminders` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2025-09-04 13:25:29
+USE petcarereminder;
+ALTER TABLE reminders
+DROP COLUMN description;
+DESC reminders;
